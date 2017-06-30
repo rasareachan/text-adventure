caf312c41eb9ad6a22e76ee4963f2cfff6b8ddff
HaiLinW <hailinw@Macs-MacBook-Pro-99.local>
2017-06-30 13:33:21 -0700

    updated version

--- feminism1.py
+++ feminism1.py
@@ -5,13 +5,17 @@ q1 = input("Do you a: 'listen to your mom' or b: 'pursue your dreams'?\n")
 
 # if choose to listen to mom
 if q1 == "a":
-    print("You will live an unhappy life.")
+    print("You will live an unhappy life.\n")
     # end game here; come back to this later lol
-
 
 # if choose to pursue dreams
 if q1 == "b":
-    print("you get it, girl!")
+    print("you get it, girl!\n")
 
 # game contiues onto q2
-print("While you are out here pursing your dreams, you also have a boyfriend of five years named Jake. He feels threatened that you are a woman going into the engineering field; he is very unhappy. Do you 'stay with him since he is your first love' or 'keep on following your dreams'?")
+print("While you are out here pursing your dreams, you also have a boyfriend of five years named Jake. He feels threatened that you are a woman going into the engineering field; he is very unhappy.\n")
+q2 = input("Do you a: 'stay with him' or 'keep doing you and following your dreams'?\n")
+
+# if choose stay with him
+if q2 == "a":
+    print("You will live an unhappy life.\n")
