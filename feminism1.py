20e131555149a64a2ce7b77a093bba0d6947ff41
HaiLinW <hailinw@Macs-MacBook-Pro-99.local>
2017-06-30 13:25:08 -0700

    updated version

--- feminism1.py
+++ feminism1.py
@@ -1,3 +1,17 @@
 name = input("Insert strong, independent name (aka your name bc you're a strong, independent woman)\n")
 
-print("Welcome " + name + "! This your life.\nYour mom wants you to be a doctor, but you've always wanted to be an engineer. Do you 'listen to your mom' or 'pursue your dreams'? (a or b)\n")
+print("Welcome " + name + "! This your life.\nYour mom wants you to be a doctor, but you've always wanted to be an engineer.\n")
+q1 = input("Do you a: 'listen to your mom' or b: 'pursue your dreams'?\n")
+
+# if choose to listen to mom
+if q1 == "a":
+    print("You will live an unhappy life.")
+    # end game here; come back to this later lol
+
+
+# if choose to pursue dreams
+if q1 == "b":
+    print("you get it, girl!")
+
+# game contiues onto q2
+print("While you are out here pursing your dreams, you also have a boyfriend of five years named Jake. He feels threatened that you are a woman going into the engineering field; he is very unhappy. Do you 'stay with him since he is your first love' or 'keep on following your dreams'?")
