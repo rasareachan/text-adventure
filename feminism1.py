c7bb55dfded66dbc654437fea40dc7f92d817d6b
RasareaC <rasareac@Macs-MacBook-Pro-99.local>
2017-06-30 11:27:11 -0700

    first file

---
+++ feminism1.py
@@ -0,0 +1,3 @@
+name = input("Insert strong, independent name (aka your name bc you're a strong, independent woman)\n")
+
+print("Welcome " + name + "! This your life.\nYour mom wants you to be a doctor, but you've always wanted to be an engineer. Do you 'listen to your mom' or 'pursue your dreams'? (a or b)\n")
