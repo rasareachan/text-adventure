90d73e37de5d73fac4cef60533d73ba201014cb7
RasareaC <rasareac@Macs-MacBook-Pro-99.local>
2017-06-30 14:07:37 -0700

    hello

--- feminism1.py
+++ feminism1.py
@@ -9,13 +9,29 @@ if q1 == "a":
     # end game here; come back to this later lol
 
 # if choose to pursue dreams
-if q1 == "b":
-    print("you get it, girl!\n")
+elif q1 == "b":
+    print("You get it, girl!\n")
+    print("While you are out here pursing your dreams, you also have a boyfriend of five years named Jake. He feels threatened that you are a woman going into the engineering field; he is very unhappy.\n")
 
-# game contiues onto q2
-print("While you are out here pursing your dreams, you also have a boyfriend of five years named Jake. He feels threatened that you are a woman going into the engineering field; he is very unhappy.\n")
-q2 = input("Do you a: 'stay with him' or 'keep doing you and following your dreams'?\n")
+    q2 = input("Do you a: 'stay with him' or b: 'keep doing you and following your dreams'?\n")
 
 # if choose stay with him
-if q2 == "a":
-    print("You will live an unhappy life.\n")
+    if q2 == "a":
+        print("You will live an unhappy life.\n")
+
+    elif q2 == "b":
+        print("Congratulations! You got rid of your obstacle. You didn't need him anyway!")
+
+        print("You graduate from college at the top of your class and land an opportunity to intern at Apple!")
+        print("However, you see that all the workers are old White men who don't understand what you're doing there as a woman?\n")
+        q3 = input("Do you a: 'get intimidated and choose to never come back to the industry' or b: 'continue to persevere through the struggles', as you've been doing, girl?\n")
+
+        if q3 == "a":
+            print("You will live an unhappy life.\n")
+
+        elif q3 == "b":
+            print("You continue with the internship and eventually get a position at the company!\n")
+            print("Five years later, you have a happy life with a supportive husband who doesn't think you threaten his masculinity!")
+            print("You are also now the CEO of your own company that teaches girls how to code!")
+            print("Your daughter sees you as an example and now wants to be as strong as you...")
+            print("You go, fam!!! You have succeeded in life, girl #empowerment")
